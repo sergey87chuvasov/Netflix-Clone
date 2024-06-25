9ec1eac5352835597e656435bc42f542e36317b7
Sergey87 <serge.techno@gmail.com>
2024-06-26 00:06:40 +0300

    Create Home Page of Netflix Website (titlecards) - 4 part

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,33 @@
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data.js';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 const TitleCards = () => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
   return (
-    <div className='titleCards'>
+    <div className='title-cards'>
       <h2>Popular on Netflix</h2>
+      <div className='card-list' ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className='card' key={index}>
+              <img src={card.image} alt='card pic' />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
