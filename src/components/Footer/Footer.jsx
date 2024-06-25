e84f1b399b3f7057a82854c21201fdc3fee5e557
Sergey87 <serge.techno@gmail.com>
2024-06-26 00:55:15 +0300

    finished homepage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,35 @@
-import './Footer.css'
+import './Footer.css';
+import youtube_icon from '../../assets/youtube_icon.png';
+import twitter_icon from '../../assets/twitter_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import facebook_icon from '../../assets/facebook_icon.png';
 
 const Footer = () => {
-
-    return (
-      <div className="footer">
-        
+  return (
+    <div className='footer'>
+      <div className='footer-icons'>
+        <img src={facebook_icon} alt='icon pic' />
+        <img src={instagram_icon} alt='icon pic' />
+        <img src={twitter_icon} alt='icon pic' />
+        <img src={youtube_icon} alt='icon pic' />
       </div>
-    )
-  }
-  
-  export default Footer;
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Informations</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>1997-2024 Netflix, Inc</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -3,7 +3,7 @@ import cards_data from '../../assets/cards/Cards_data.js';
 import { useRef } from 'react';
 import { useEffect } from 'react';
 
-const TitleCards = () => {
+const TitleCards = ({ title, category }) => {
   const cardsRef = useRef();
 
   const handleWheel = (event) => {
@@ -17,7 +17,7 @@ const TitleCards = () => {
 
   return (
     <div className='title-cards'>
-      <h2>Popular on Netflix</h2>
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
       <div className='card-list' ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
