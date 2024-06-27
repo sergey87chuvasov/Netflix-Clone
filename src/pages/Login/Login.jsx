f06f15aa71d4556c530122d564033551ef1495cb
Sergey87 <serge.techno@gmail.com>
2024-06-27 23:48:41 +0300

    finished Login Page of Netflix Clone Website

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,24 @@
 import './Login.css';
 import logo from '../../assets/logo.png';
+import { useState } from 'react';
 
 const Login = () => {
+  const [signState, setSignState] = useState('Sign In');
   return (
     <div className='login'>
       <img src={logo} alt='logo pic' className='login-logo' />
       <div className='login-form'>
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type='text' placeholder='Your name' />
+          {signState === 'Sign Up' ? (
+            <input type='text' placeholder='Your name' />
+          ) : (
+            <></>
+          )}
+
           <input type='email' placeholder='Email' />
           <input type='password' placeholder='Password' />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className='form-help'>
             <div className='remember'>
               <input type='checkbox' />
@@ -20,6 +27,31 @@ const Login = () => {
             <p>Need Help?</p>
           </div>
         </form>
+        <div className='form-switch'>
+          {signState === 'Sign In' ? (
+            <p>
+              New to Netflix?{' '}
+              <span
+                onClick={() => {
+                  setSignState('Sign Up');
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{' '}
+              <span
+                onClick={() => {
+                  setSignState('Sign In');
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
