d26f78f6a11e48cbdbf3aabcbeece3221030c6c4
Sergey87 <serge.techno@gmail.com>
2024-06-27 23:12:26 +0300

    Create Login Page of Netflix Clone Website - 1 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Home from "./pages/Home/Home";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,28 @@
-import './Login.css'
+import './Login.css';
+import logo from '../../assets/logo.png';
 
 const Login = () => {
-
-
-    return (
-      <div className="login">
-        
+  return (
+    <div className='login'>
+      <img src={logo} alt='logo pic' className='login-logo' />
+      <div className='login-form'>
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' placeholder='Your name' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <button>Sign Up</button>
+          <div className='form-help'>
+            <div className='remember'>
+              <input type='checkbox' />
+              <label>Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
       </div>
-    )
-  }
-  
-  export default Login
-  
+    </div>
+  );
+};
+
+export default Login;
