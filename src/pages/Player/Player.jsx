f14331bc4201b413f919ec14e1bad9f543017685
Sergey Chv <serge.techno@gmail.com>
2024-06-29 12:04:28 +0300

    Create video player page - 1 part

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,11 +1,19 @@
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
 
-
     return (
       <div className="player">
-        
+        <img src={back_arrow_icon} alt="icon pic" />
+        <iframe width='90%' height='90%' src='https://www.youtube.com/watch?v=wqIzNDSutSI' title='trailer'
+        frameBorder='0' allowFullScreen>
+          <div className="player-info">
+            <p>Published Date</p>
+            <p>Name</p>
+            <p>Type</p>
+          </div>
+        </iframe>
       </div>
     )
   }
