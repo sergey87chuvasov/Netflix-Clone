996e7b1f999f619bac23ad171572ebb1d6c7d4e7
Sergey87 <serge.techno@gmail.com>
2024-06-30 23:13:00 +0300

    finished video player page

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,47 +1,63 @@
-import './Player.css'
-import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import './Player.css';
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
 import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Player = () => {
+  const { id } = useParams();
+
+  const navigate = useNavigate();
 
   const [apiData, setApiData] = useState({
     name: '',
     key: '',
     published_at: '',
-    typeof: ''
-  })
+    typeof: '',
+  });
 
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZGMzNmMzOTM0YzJkYWY2YTViNmMzM2ZjN2I1YjdiOCIsIm5iZiI6MTcxOTY1MjQzNy4xNTEzMDcsInN1YiI6IjY2N2RkMGQ3ZmE1MTQzZTNjMWNjOTY4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YgQT8tZS558VbZj1y3Txm8pXY1RqJCai7kE2z0GpEWU'
-    }
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZGMzNmMzOTM0YzJkYWY2YTViNmMzM2ZjN2I1YjdiOCIsIm5iZiI6MTcxOTY1MjQzNy4xNTEzMDcsInN1YiI6IjY2N2RkMGQ3ZmE1MTQzZTNjMWNjOTY4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YgQT8tZS558VbZj1y3Txm8pXY1RqJCai7kE2z0GpEWU',
+    },
   };
 
   useEffect(() => {
-
-    fetch('https://api.themoviedb.org/3/movie/1022789/videos?language=en-US', options)
-    .then(response => response.json())
-    .then(response => setApiData(response.results[0]))
-    .catch(err => console.error(err));
-
-  },[])
-  
- 
-    return (
-      <div className="player">
-        <img src={back_arrow_icon} alt="icon pic" />
-        <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer'
-        frameBorder='0' allowFullScreen></iframe>
-          <div className="player-info">
-            <p>{apiData.published_at.slice(0,10)}</p>
-            <p>{apiData.name}</p>
-            <p>{apiData.type}</p>
-          </div>
-      </div>
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
     )
-  }
-  
-  export default Player
-  
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className='player'>
+      <img
+        src={back_arrow_icon}
+        alt='icon pic'
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width='90%'
+        height='90%'
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title='trailer'
+        frameBorder='0'
+        allowFullScreen
+      ></iframe>
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
