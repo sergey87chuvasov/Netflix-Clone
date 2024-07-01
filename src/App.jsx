779c6c3b0c04b0a44ec64ec7521e196a2df0e23f
Sergey87 <serge.techno@gmail.com>
2024-07-01 22:43:08 +0300

    Create Login and Registration feature using Firebase - 2 part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Player from './pages/Player/Player';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log('log in');
+      } else {
+        console.log('log out');
+      }
+    });
+  }, []);
   return (
     <div>
       <Routes>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,24 @@
 import './Login.css';
 import logo from '../../assets/logo.png';
 import { useState } from 'react';
+import { login, signup } from '../../firebase.js';
 
 const Login = () => {
   const [signState, setSignState] = useState('Sign In');
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const user_auth = async (e) => {
+    e.preventDefault();
+    if (signState === 'Sign In') {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+  };
+
   return (
     <div className='login'>
       <img src={logo} alt='logo pic' className='login-logo' />
@@ -11,14 +26,37 @@ const Login = () => {
         <h1>{signState}</h1>
         <form>
           {signState === 'Sign Up' ? (
-            <input type='text' placeholder='Your name' />
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type='text'
+              placeholder='Your name'
+            />
           ) : (
             <></>
           )}
 
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
-          <button>{signState}</button>
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type='email'
+            placeholder='Email'
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type='password'
+            placeholder='Password'
+          />
+          <button onClick={user_auth} type='submit'>
+            {signState}
+          </button>
           <div className='form-help'>
             <div className='remember'>
               <input type='checkbox' />
